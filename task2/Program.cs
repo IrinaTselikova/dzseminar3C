4542e73d9b523f59ffea7b1096e70af05be2f8f8
unknown <hurina.irina@mail.ru>
2022-10-14 08:51:33 +0300

    Task 2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 21
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53 */
+
+Point GetPoint()
+{
+Point point = new Point();
+Console.WriteLine("Введите координату Х:");
+point.X = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координату У:");
+point.Y = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координату Z:");
+point.Z = int.Parse(Console.ReadLine());
+
+return point;
+}
+
+Point firstPoint = GetPoint();
+Point secondPoint = GetPoint();
+
+double result = Math.Sqrt(Math.Pow(secondPoint.X - firstPoint.X,2) + Math.Pow(secondPoint.Y - firstPoint.Y,2) + Math.Pow(secondPoint.Z - firstPoint.Z,2));
+Console.WriteLine(result);
+
+class Point
+{
+public int X { get; set;}
+public int Y { get; set;}
+public int Z { get; set;}
+}
+
+
